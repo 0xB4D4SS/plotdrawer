b89eee084fd5c9db385edd8baf9e5627c112059f
0xB4D4SS <0xb4d4ss@gmail.com>
2021-01-20 10:28:43 +0400

    added 3d projection
    now you can choose: draw plot in 2d or 3d

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from numpy import *
 
 
@@ -11,6 +12,7 @@ class Example(QWidget):
         super().__init__()
 
         self.btn = QPushButton('Draw plot', self)
+        self.rbtn = QRadioButton("draw in 3d", self)
         self.picture = QPixmap('plot.png')
         self.label = QLabel('Input expression:', self)
         self.input = QLineEdit('', self)
@@ -21,6 +23,8 @@ class Example(QWidget):
 
         self.btn.move(50, 100)
         self.btn.clicked.connect(self.onclick)
+
+        self.rbtn.move(50, 140)
 
         self.label.move(50, 30)
 
@@ -33,12 +37,22 @@ class Example(QWidget):
 
     def onclick(self):
         formula = self.input.text()
-        step = 0.1
-        x = arange(-10.0, 10.0, step)
-        print(eval(formula))
-        y = eval(formula)
-        fig = plt.figure()
-        plt.plot(x, y)
+        if (self.rbtn.isChecked()):
+            x = linspace(-10.0, 10.0)
+            y = x
+            z = eval(formula)
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot(x, y, z)
+
+        else:
+            step = 0.1
+            x = arange(-10.0, 10.0, step)
+            print(eval(formula))
+            y = eval(formula)
+            fig = plt.figure()
+            plt.plot(x, y)
+
         plt.savefig('plot.png')
         plt.show()
 
