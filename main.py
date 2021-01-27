ded6db597d2c14be937adc0fae499b849e801963
0xB4D4SS <0xb4d4ss@gmail.com>
2021-01-27 12:24:55 +0400

    working on breakpoints of plot
    in 2d sqrt, log and abs(x) crash program;
    in 3d there is no breakpoints fix now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from numpy import *
+from sympy import limit, sympify, symbols
 
 
 class GUI(QWidget):
@@ -38,37 +39,84 @@ class GUI(QWidget):
         self.show()
 
     def onclick(self):
+        step = 0.1
         # получаем текст функции для отрисовки
         formula = self.finput.text()
         # 3d случай
         if self.rbtn.isChecked():
-            # задаем функцию неявно (z = f(x,y)) с помощью лямбда выражения
-            f = lambda x, y: eval(formula)
             # задаем диапазоны x и y
-            xval = linspace(-2*pi, 2*pi, 500)
-            yval = linspace(-2*pi, 2*pi, 500)
+            xval = arange(-6.0, 6.0, step)
+            yval = arange(-6.0, 6.0, step)
             x, y = meshgrid(xval, yval)
-            # дебажный вывод в консоль
-            print(eval(formula))
+            # задаем функцию неявно (z = f(x,y)) с помощью лямбда выражения
+            f = lambda x=0, y=0: eval(formula)
+            # if formula.find('x') != -1:
+            #     i = -1
+            #     for valx in xval:
+            #         i = i + 1
+            #         xs = symbols('x')
+            #         lim1 = limit(sympify(formula), xs, valx, dir='-').evalf()
+            #         lim2 = limit(sympify(formula), xs, valx, dir='+').evalf()
+            #         limnext = limit(sympify(formula), xs, valx - step, dir='+').evalf()
+            #         currz = f(valx, yval[i])
+            #         if not(lim1 - lim2 < step) or not(lim1 - currz < step) or not(lim2 - currz < step):
+            #             xval[i] = nan
+            #         elif diff([lim2, limnext]) > 20.0:
+            #             xval[i] = nan
+            # if formula.find('y') != -1:
+            #     k = -1
+            #     for valy in yval:
+            #         k = k + 1
+            #         ys = symbols('y')
+            #         lim3 = limit(sympify(formula), ys, valy, dir='-').evalf()
+            #         lim4 = limit(sympify(formula), ys, valy, dir='+').evalf()
+            #         limnext2 = limit(sympify(formula), ys, valy - step, dir='+').evalf()
+            #         currz = f(xval[k], valy)
+            #         if not(lim3 - lim4 < step) or not(lim3 - currz < step) or not(lim4 - currz < step):
+            #             yval[k] = nan
+            #         elif diff([lim4, limnext2]) > 20.0:
+            #             yval[k] = nan
+            # print('x')
+            # print(xval)
+            # print('y')
+            # print(yval)
+            # print(eval(formula))
             z = f(x, y)
             # собсна создаем график
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
-            ax.plot_surface(x, y, z, cmap=cm.coolwarm, cstride=30, rstride=30)
+            ax.plot_surface(x, y, z, cstride=10, rstride=10)
             # границы отрисовки графика
             # ((y^2)/x до сих пор странно рисуется, в остальном работает)
             ax.set_zlim(-30, 30)
         # 2d случай
         else:
+            # задаем диапазон x
+            xval = arange(-6.0, 6.0, step)
             # выражаем y как функцию от x с помощью лямбда-выражения
             y = lambda x: eval(formula)
-            # задаем диапазон x
-            x = linspace(-2*pi, 2*pi, 1000)
+            # проверяем функцию на точки разрыва
+            # проверяем равенство пределов и значения функции в точке и проверяем нету ли скачка предела в точке
+            # если пределы равны или скачка нет - точки разрыва нет, иначе есть
+            # до сих пор не рисует sqrt, log и abs
+            k = -1
+            for val in xval:
+                k = k+1
+                x = symbols('x')
+                lim1 = limit(sympify(formula), x, val, dir='-').evalf()
+                lim2 = limit(sympify(formula), x, val, dir='+').evalf()
+                limnext = limit(sympify(formula), x, val-step, dir='+').evalf()
+                if not(lim1-lim2 < step) or not(lim1 - y(val) < step) or not(lim2 - y(val) < step):
+                    xval[k] = nan
+                elif diff([lim2, limnext]) > 20.0:
+                    xval[k] = nan
+            print(xval)
             # дебажный вывод в консоль
-            print(eval(formula))
+            # print(eval(formula))
             # собсна создаем график
             fig = plt.figure()
-            plt.plot(x, y(x))
+            plt.plot(xval, y(xval))
+            # пределы отрисовки оси y
             plt.ylim(-5, 5)
 
         plt.grid()
